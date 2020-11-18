05e3184e3952807547dddad385a31b34dc4d6ce5
Joshua <joshuamacleod@live.com>
2020-11-18 21:57:38 +0000

    Finised setting up login and logout function

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  root 'home/index'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-
-
+  root :to => 'home#index'
+  # make this a habbit to white our roots
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-
-  get 'signup', to: 'users/new', as: 'signup'
-  get 'login', to: 'sessions/new', as: 'login'
-  get 'logout', to: 'sessions/destroy', as: 'logout'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :posts
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
+
+# undefined local variable or method `‘home' for #<ActionDispatch::Routing::Mapper:0x00007ffcb9b94160> (NameError)
+# this was caused because of lack of consistent indentation ''"" and even farious other forms of dashes that are caused by style type in different computers
+
+# Missing :controller key on routes definition, please check your routes. (ArgumentError)
+# The main problem is that you need # instead of / in your routes.
+
+# (brief explination )
+# https://stackoverflow.com/questions/42846674/missing-controller-key-on-routes-definition-please-check-your-routes/49762666
