66f1089f794e3dd7b1a3a18fec36a009adf170db
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-11-18 16:59:59 +0000

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  root 'home/index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+
+
   resources :users
-  get 'home/index'
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'signup', to: 'users/new', as: 'signup'
+  get 'login', to: 'sessions/new', as: 'login'
+  get 'logout', to: 'sessions/destroy', as: 'logout'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
