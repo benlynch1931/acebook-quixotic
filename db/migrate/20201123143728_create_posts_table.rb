94acfaf92574effad875238e5eda1e05bece401c
Ben <ben.lynch1931@gmail.com>
2020-11-26 09:19:35 +0000

    chore: migrate database to include first and last name of users

--- db/migrate/20201123143728_create_posts_table.rb
+++
@@ -1,9 +0,0 @@
-class CreatePostsTable < ActiveRecord::Migration[5.1]
-  def change
-    create_table :posts_tables do |t|
-      t.text :message
-      add_foreign_key :posts, :users
-      t.timestamps null: false
-    end
-  end
-end
